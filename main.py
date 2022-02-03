bdf98a3064ac3eadd76c8d613c8407e5240f509a
Shakti Dagar <shaktidagar09@gmail.com>
2022-02-04 04:53:10 +0530

    dynamic routing

--- main.py
+++ main.py
@@ -9,6 +9,19 @@ def index():
     return {"data": {"name": "Shakti"}}
 
 
-@app.get("/about")
-def about():
-    return {"data": "about page"}
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"data": "all unpublished blogs"}
+
+
+# we put id in curly-brackets,so that we can use it as a variable in path opration functoin (def abc()).
+# this is called dynamic routing.
+@app.get("/blog/{id}")
+# passed id in function
+def blog(id: int):
+    return {"data": id}
+
+
+@app.get("/blog/{id}/comments")
+def comments(id: int):
+    return {"data": {"1", "2"}}
